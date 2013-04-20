1444b9865a5a2e2d66a85241daa2d3a4a4458ef0
278019 <278019>
2013-04-20 10:50:22 +0300

    3.04

--- canon68452/index.php
+++ canon68452/index.php
@@ -95,7 +95,7 @@
 	<link href="/css/bootstrap-modal.css" rel="stylesheet" type="text/css"/>
 	<script src="/js/bootstrap-modalmanager.js"></script>
 	<script src="/js/bootstrap-modal.js"></script>
-	<script type="text/javascript" src="js/ajaxAdmin.js"></script>
+	<script type="text/javascript" src="/canon68452/ajax/ajaxAdmin.js"></script>
 
 		<script type="text/javascript">
 		function confirmDelete() {
